33c7b81488061d2918ba89eaacac53816923a274
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-05-13 15:58:14 +0530

    Added two different strategies

--- java/src/services/fee/HourlyBasedStrategy.java
+++ java/src/services/fee/HourlyBasedStrategy.java
@@ -2,9 +2,14 @@ package services.fee;
 
 import entities.Ticket;
 
+import java.util.Objects;
+
 public class HourlyBasedStrategy implements FeeStrategy{
     @Override
     public double calculateFee(Ticket ticket) {
-        return 0;
+        long entryTime = ticket.getEntryTime().getTime();
+        long exitTime = (Objects.nonNull(ticket.getExitTime()))?ticket.getExitTime().getTime():System.currentTimeMillis();
+        long durationInHours = (exitTime - entryTime) / (1000 * 60 * 60);
+        return durationInHours * 10; // Assuming $10 per hour
     }
 }
--- java/src/services/fee/MinuteBasedFeeStrategy.java
+++ java/src/services/fee/MinuteBasedFeeStrategy.java
@@ -5,6 +5,9 @@ import entities.Ticket;
 public class MinuteBasedFeeStrategy implements FeeStrategy{
     @Override
     public double calculateFee(Ticket ticket) {
-        return 0;
+        long entryTime = ticket.getEntryTime().getTime();
+        long exitTime = (ticket.getExitTime() != null) ? ticket.getExitTime().getTime() : System.currentTimeMillis();
+        long durationInMinutes = (exitTime - entryTime) / (1000 * 60);
+        return durationInMinutes * 0.5; // Assuming $0.5 per minute
     }
 }
