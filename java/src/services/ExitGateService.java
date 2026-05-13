1fac4d49e8de3deffaf1211253e5a681739db1d9
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-05-13 16:25:29 +0530

    Added exit gate logic

--- java/src/services/ExitGateService.java
+++ java/src/services/ExitGateService.java
@@ -1,14 +1,31 @@
 package services;
 import entities.*;
+import constants.*;
+import services.fee.FeeStrategy;
+import services.fee.HourlyBasedStrategy;
+import services.fee.MinuteBasedFeeStrategy;
 
 import java.io.*;
 import java.util.*;
 
+import static constants.FeeStrategyEnum.HOUR;
+import static constants.FeeStrategyEnum.MINUTE;
+
 public class ExitGateService {
 
-    public String freeSpot(Ticket ticket){
+    public String freeSpot(Ticket ticket,FeeStrategyEnum feeStrategy){
         ParkingSpot spot = ticket.getParkingSpot();
         spot.setOccupied(false);
-        return "";
+
+        FeeStrategy feeCalculator;
+
+        feeCalculator = switch (feeStrategy) {
+            case HOUR -> new HourlyBasedStrategy();
+            case MINUTE -> new MinuteBasedFeeStrategy();
+        };
+
+        double fee = feeCalculator.calculateFee(ticket);
+
+        return "Vehicle with license plate " + ticket.getVehicle().getId() + " has exited. Total fee: $" + fee;
     }
 }
